98a63e636f423e76d32f70674e218fb2c473d937
lip47 <jonasvivier@gmail.com>
2026-01-06 11:30:01 +0000

    Ajout dossier exercice, index.html, index.js + réalisation de l'exercice1

---
+++ exercice-variables/index.js
@@ -0,0 +1,31 @@
+const character = "Legolas";
+
+console.log(character);
+
+let name = "Legolas";
+
+console.log(name);
+
+name = "Gimli";
+
+console.log(name);
+
+let nbentier = 15345341215;
+
+console.log(nbentier);
+
+let nbdecimal = 13.1215451215;
+
+console.log(nbdecimal);
+
+let nbnegatif = -1121542121;
+
+console.log(nbnegatif);
+
+let yes = true;
+
+console.log(yes);
+
+let no = false;
+
+console.log(no);
